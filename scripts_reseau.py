ea4247737433a4822c3d6d4fd3595f86ae7d96ce
Alyasa BELFAR <compteciel0@proton.me>
2026-09-21 17:45:52 +0200

    tp1

--- scripts_reseau.py
+++ scripts_reseau.py
@@ -1,3 +1,15 @@
 # TP Git et Python - Binôme : [BELFAR] [Alyasa] et [GUEZAMIR] [Redwane]
 # Date : [21/09/2026]
                                     
+ip1 = "192.168.1.1"
+ip2 = "10.0.0.1"
+
+ip1, ip2 = ip2, ip1
+
+print("ip1 =", ip1)
+print("ip2 =", ip2)
+
+TERMINAL 
+
+ip1 = 10.0.0.1
+ip2 = 192.168.1.1
