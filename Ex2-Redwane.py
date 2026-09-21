83972e2f7ae8f867ff4d97ef2c8ac8292be53c60
Tawfil <monmail@gmail.com>
2026-09-21 17:46:55 +0200

    Exo2

---
+++ Ex2-Redwane.py
@@ -0,0 +1,9 @@
+nombre1 = int(input("Chiffre numero 1 : "))
+nombre2 = int(input("Chiffre numero 2 : "))
+
+somme = nombre1 + nombre2
+moyenne = (nombre1 + nombre2) / 2
+
+print(f"la somme de {nombre1} et {nombre2} est {somme}" )
+
+print(f"La moyenne de {nombre1} et {nombre2} est {moyenne} ")
